837be8c59f1b8a34815e4aba92a0c42970c51d04
zipmishahl2 <110753825+zipmishahl2@users.noreply.github.com>
2024-02-14 14:05:57 +0700

    Update app.js

--- api/source/app.js
+++ api/source/app.js
@@ -1,26 +1,19 @@
-<<<<<<< HEAD
 //# copyright © 2022 Reboot Team || the license is created automatically after the file is created.
-
-//# starting api
-=======
-/*copyright © 2022 Reboot Team*/
 /* hl2reboot-site 0.11*/
 /*
  * @license true
  * @author hl2reboot
  * @license: http://www.apache.org/licenses/LICENSE-2.0
  */
->>>>>>> 5a20a1fffcf24481c7835f26c248a46686b1d6da
+
+//# starting api
 import * as settings from "/api/source/settings.js";
 import * as web from "/api/source/web.js";
 const hl2reboot = {
     "rest.api": true,
     "verison hl2reboot site": "0.11",
-<<<<<<< HEAD
     "verison API": "2.3",
-=======
     "verison API": "2.2",
->>>>>>> 5a20a1fffcf24481c7835f26c248a46686b1d6da
     "description": "Scientist John can save the world",
     "apiEnable": true,
     "telemetryEnabled": false,
@@ -29,13 +22,10 @@ const hl2reboot = {
     "main ru": "ru.html"
     }
 
-<<<<<<< HEAD
 const server = {
     name: "rest.api",
     update_site: "2.3",
     updatedAt: "2024-02-14T7:00:23.986Z",
     server_status: "http://localhost:5500"
 }
-=======
 {"name": "rest.api", "update": 2.2, "updatedAt": "2024-02-14T1:01:23.986Z"}
->>>>>>> 5a20a1fffcf24481c7835f26c248a46686b1d6da
