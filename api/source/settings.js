4dc1d0e156c696f123776659ec4b32e1258d9e61
Misha <110753825+zipmishahl2@users.noreply.github.com>
2024-02-14 14:03:27 +0700

    create files and upload

---
+++ api/source/settings.js
@@ -0,0 +1,29 @@
+/**copyright © 2022 Reboot Team
+the license is created automatically after the file is created.*/
+/** hl2reboot-site 0.11
+ * @license true
+ * @author hl2reboot
+ * @license: http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import * as settings from "/js/app.js";
+const settings = {
+  telemetryEnabled: false,
+  cookiesEnabled: false,
+  hostingEnabled: true
+}
+
+const hosting_site = {
+  telemetryEnabled: false,
+  cookiesEnabled: false,
+  restapi: true,
+  optimizationEnabled: true
+}
+
+const config_site = {
+  name_site_eu: "HL2:Reboot",
+  name_site_ru: "ХЛ2:Ребут",
+  telemetryEnabled: false,
+  cookiesEnabled: false,
+  optimizationEnabled: true
+}
