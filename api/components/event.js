f3b6ac4cea900b8454c55e1c70ad2d0a46c6b6f1
zipmishahl2 <110753825+zipmishahl2@users.noreply.github.com>
2024-03-12 02:34:43 +0700

    Update event.js

--- api/components/event.js
+++ api/components/event.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var oldVersion = localStorage.getItem("APIVersion") || "";
+  var currentVersion = "3.0";
 
+  if (oldVersion !== currentVersion) {
+    // Changes to the API version
+    console.log("The API version has changed");
+
+    // Adding a new version to localStorage
+    localStorage.setItem("APIVersion", currentVersion);
+  }
+});
