2a7e1424040ef21a8b55dfff3387f1f0ab7cb9be
zipmishahl2 <110753825+zipmishahl2@users.noreply.github.com>
2024-03-12 01:41:35 +0700

    Create time.js

---
+++ api/components/time.js
@@ -0,0 +1,4 @@
+var time = setInterval(function() {
+  var date = new Date();
+  document.getElementById("time").innerHTML = (date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+}, 1000)
