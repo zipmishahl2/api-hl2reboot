6e1132b305e37b4a1294db7d58507cce805004b4
Misha <110753825+zipmishahl2@users.noreply.github.com>
2024-03-11 19:38:05 +0700

    v3.0

--- README.md
+++
@@ -1,4 +0,0 @@
-> [!WARNING]
-> you are seeing this message because the new update will result in speed and low latency v3.0 hl2reboot.api
-
-> It is not yet clear when the update for the API will be released
---
+++ api/pages/api/data/socials.js
@@ -0,0 +1,17 @@
+export const socials = [
+  {
+    id: 1,
+    icon: 'discord',
+    path: 'https://discord.gg/7WakPvSENV',
+  },
+  {
+    id: 2,
+    icon: 'twitter',
+    path: 'https://twitter.com/Hl2Reboot37147',
+  },
+  {
+    id: 3,
+    icon: 'youtube',
+    path: 'https://youtube.com/@hl2rebootTeam',
+  },
+];
