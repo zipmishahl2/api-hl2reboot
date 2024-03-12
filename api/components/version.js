e870eb814a05ec1fb8b91d261cbeba03cff7ab1f
zipmishahl2 <110753825+zipmishahl2@users.noreply.github.com>
2024-03-12 15:48:44 +0700

    Update version.js

--- api/components/version.js
+++ api/components/version.js
@@ -1,33 +1,14 @@
-{
-  name:"hl2reboot.api",
-  version_api:"3.0"
+var salaries = {
+    "hl2reboot.api": 3.0,
+    "hl2reboot.vercel.app": 5.0
+};
+
+function changeObject(object) {
+    for (var verison in object) {
+        object[verison] += 100;
+    }
 }
 
-{
-  name:"hl2reboot.vercel.app",
-  version_site:"5.0"
-}
-
-// Create the event
-var event = new CustomEvent("verison", { "detail": "Test verison" });
-
-// Dispatch/Trigger/Fire the event
-document.dispatchEvent(event);
-
-var event; // The custom event that will be created
-if(document.createEvent){
-    event = document.createEvent("HTMLEvents");
-    event.initEvent("version", true, true);
-    event.eventName = "version";
-    element.dispatchEvent(event);
-} else {
-    event = document.createEventObject();
-    event.eventName = "verison";
-    event.eventType = "verison";
-    element.fireEvent("on" + event.eventType, event);
-}
-
-// Add an event listener
-document.addEventListener("name-of-event", function(e) {
-  console.log(e.detail); // Prints "Example of an event"
-});
+console.dir(salaries);
+changeObject(salaries);
+console.dir(salaries);
