73639a382b405ac110712d04a588d78d39db1450
zipmishahl2 <110753825+zipmishahl2@users.noreply.github.com>
2024-03-13 23:35:18 +0700

    Update version.js

--- api/components/version.js
+++ api/components/version.js
@@ -1,8 +1,7 @@
-const nameAPI = { name: 'hl2reboot.api' };
-const nameSite = { name: 'hl2reboot.vercel.app' };
-const verisonAPI = { name: '3.0' };
-const verisonSite = { name: '5.0' };
+let body = document.querySelector('body');
+const company = { name: 'ShonikSoftware Team' };
+const nameAPI = { name: 'hl2reboot.api version 3.0' };
+const nameSite = { name: 'hl2reboot.vercel.app version 5.0' };
+console.log(company);
 console.log(nameAPI);
 console.log(nameSite);
-console.log(versionAPI);
-console.log(versionSite);
